41cb9544fc5713f21a0722a2e9edf13b8bcfc76c
MAngelWar <mangelwar@gmail.com>
2025-03-14 21:38:53 -0400

    updated

--- script/c1246828.lua
+++ script/c1246828.lua
@@ -1,4 +1,130 @@
 --Firewall Dragon Paradoxtroyer
-function c1246828.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--Requisitos de Invocación por Enlace
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),4,99,s.matfilter)
+
+	--Destruir todas las demás cartas al ser Invocado por Enlace
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.descon)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+
+	--Devolver una carta al mazo y atacar directamente
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.rettg)
+	e2:SetOperation(s.retop)
+	c:RegisterEffect(e2)
+
+	--Duplicar ATK durante la batalla
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e3:SetCondition(s.atkcon)
+	e3:SetValue(s.atkval)
+	c:RegisterEffect(e3)
+
+	--Negar activación de una carta o efecto
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_DISABLE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetCondition(s.negcon)
+	e4:SetTarget(s.negtg)
+	e4:SetOperation(s.negop)
+	c:RegisterEffect(e4)
+end
+
+--Material Link: Debe incluir al menos 1 Monstruo de Enlace
+function s.matfilter(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsType,1,nil,TYPE_LINK,lc,sumtype,tp)
+end
+
+--Condición de destrucción: Solo si es Invocado por Enlace
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+
+--Seleccionar todas las demás cartas en el campo
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+
+--Destruir todas las demás cartas en el campo
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.Destroy(g,REASON_EFFECT)
+end
+
+--Seleccionar 1 carta en el campo para devolver a la mano
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+
+--Devolver a la mano y permitir ataque directo
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+		--Permitir ataque directo
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e:GetHandler():RegisterEffect(e1)
+	end
+end
+
+--Condición para duplicar ATK en batalla
+function s.atkcon(e)
+	local tp=e:GetHandlerPlayer()
+	local d=Duel.GetAttackTarget()
+	return Duel.GetAttacker()==e:GetHandler() and d and d:IsControler(1-tp) and d:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+
+--Duplicar ATK durante el cálculo de daño
+function s.atkval(e,c)
+	return c:GetAttack()*2
+end
+
+--Condición para negar activación
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsChainNegatable(ev)
+end
+
+--Seleccionar la carta a negar
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+
+--Negar efecto y evitar que cartas con el mismo nombre se activen
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		local c=re:GetHandler()
+		--Evitar activaciones futuras del mismo nombre
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetTargetRange(LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK+LOCATION_REMOVED,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK+LOCATION_REMOVED)
+		e1:SetTarget(function(_,c) return c:IsCode(c:GetOriginalCode()) end)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
 end
